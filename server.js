8c0517ecadba3b2782d68003b59a1afbffc53866
Victor Shinya <contato@victorshinya.com.br>
2019-10-21 23:48:22 -0300

    Fix session renewal

--- server.js
+++ server.js
@@ -16,29 +16,48 @@ const assistant = new AssistantV2({
 
 app.post('/api/assistant', async (req, res) => {
     if (!req.body.session_id) {
-        const sessionResponse = await assistant.createSession({
-            assistant_id: process.env.ASSISTANT_ID
-        });
-        console.log(`session_id: ${sessionResponse.session_id}`);
-        req.body.session_id = sessionResponse.session_id;
+        req.body.session_id = await generateNewSession();
     }
     console.log(`params: ${JSON.stringify(req.body)}`);
+    try {
+        const response = await sendMessage(req.body);
+        res.json(response);
+    } catch (e) {
+        console.log(`throw an error: ${e.message}`);
+        if (e.code === 404) {
+            req.body.session_id = await generateNewSession();
+            console.log(`params: ${JSON.stringify(req.body)}`);
+            const response = await sendMessage(req.body);
+            res.json(response);
+        }
+    }
+});
+
+generateNewSession = async () => {
+    const sessionResponse = await assistant.createSession({
+        assistant_id: process.env.ASSISTANT_ID
+    });
+    console.log(`session_id: ${sessionResponse.session_id}`);
+    return sessionResponse.session_id;
+}
+
+sendMessage = async (body) => {
     const response = await assistant.message({
         assistant_id: process.env.ASSISTANT_ID,
-        session_id: req.body.session_id,
+        session_id: body.session_id,
         input: {
-            text: req.body.text,
+            text: body.text,
             options: {
                 return_context: true,
             }
         },
-        context: req.body.context
+        context: body.context
     });
-    response.session_id = req.body.session_id;
-    res.json(response);
-})
+    response.session_id = body.session_id;
+    return response;
+}
 
 const port = process.env.PORT || '8080';
 app.listen(port, () => {
     console.log(`Picard is up and running at port ${port}`);
-})
+});
